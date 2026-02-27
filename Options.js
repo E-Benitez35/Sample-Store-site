afe4ad64340a6189ea9c651f1106062fdf5b0948
ShadowE35 <benitezeric210@gmail.com>
2026-02-27 00:26:57 -0600

    Third commit
    Made some improvements to the web page.

--- Options.js
+++ Options.js
@@ -2,42 +2,42 @@
 
 var item = [
     {
-        "option" : "#1",
+        "option" : "Item1",
         "tag" : "Description"
     },
 
     {
-        "option" : "#2",
+        "option" : "Item2",
         "tag" : "Description"
     },
 
     {
-        "option" : "#3",
+        "option" : "Item3",
         "tag" : "Description"
     },
 
     {
-        "option" : "#4",
+        "option" : "Item4",
         "tag" : "Description"
     },
 
     {
-        "option" : "#5",
+        "option" : "Item5",
         "tag" : "Description"
     },
 
     {
-        "option" : "#6",
+        "option" : "Item6",
         "tag" : "Description"
     },
 
     {
-        "option" : "#7",
+        "option" : "Item7",
         "tag" : "Description"
     },
 
     {
-        "option" : "#8",
+        "option" : "Item8",
         "tag" : "Description"
     }
 
@@ -48,11 +48,39 @@ const OptionsFill = document.getElementById("Variety");
 
 item.forEach(item =>
 {
-    var fill = document.createElement("div");
+    //Create div for displaying array in HTML
+    const fill = document.createElement("div");
+    //fill.className will assign Name to fill div variable
+    fill.className = "Item";
 
-    fill.innerHTML = `<h1> ${item.option} </h1>
+    //Create div for displaying inner part of card display
+    const innerfill = document.createElement("div");
+    //fill.className will assign Name to fill div variable
+    innerfill.className = "Inner";
+
+    //front
+    const front = document.createElement("div");
+    front.className = "Front";
+    //Format Items from array to HTML
+    front.innerHTML = `<h1> ${item.option} </h1>
     <p> ${item.tag}</p>`;
 
+    //back
+    const back = document.createElement("div");
+    back.className = "Back";
+    //Format Items from array to HTML
+    back.innerHTML = `<img src="Images/Image.png" alt="Image" id="Image" />`;
+
+    //Append Items from arrayu to HTML
+    innerfill.appendChild(front);
+    innerfill.appendChild(back);
+    fill.appendChild(innerfill);
     OptionsFill.appendChild(fill);
+
+    //only used when wanting to flip object on click instead of hover
+    //fill.addEventListener("click", () =>
+    //{
+        //fill.classList.toggle('flipped');
+    //});
 }    
-)
+);
--- Order.js
+++ Order.js
@@ -1,43 +1,43 @@
 "use strict"
-
+//Display Available Stock
 var Litem = [
     {
-        "option" : "1",
+        "option" : "Item1",
         "tag" : "2"
     },
 
     {
-        "option" : "2",
+        "option" : "Item2",
         "tag" : "4"
     },
 
     {
-        "option" : "3",
+        "option" : "Item3",
         "tag" : "8"
     },
 
     {
-        "option" : "4",
+        "option" : "Item4",
         "tag" : "10"
     },
 
     {
-        "option" : "5",
+        "option" : "Item5",
         "tag" : "1"
     },
 
     {
-        "option" : "6",
+        "option" : "Item6",
         "tag" : "2"
     },
 
     {
-        "option" : "7",
+        "option" : "Item7",
         "tag" : "3"
     },
 
     {
-        "option" : "8",
+        "option" : "Item8",
         "tag" : "4"
     }
 
@@ -46,30 +46,81 @@ var Litem = [
 //Options
 const OptionsFill = document.getElementById("Calculator");
 
+//Function will append items from table above to html page in format below
 Litem.forEach((item, index) =>
 {
     var fill = document.createElement("div");
 
     fill.innerHTML = `<h1> ${item.option} </h1>
     <p> $${item.tag}</p>
-    <input type = "number" min = "0" id = "qty${index}" </input>`;
+    <input type = "number" min = "0" id = "qty${index}" oninput="Subtotal(), Quantity()">`; //oninput allows for live changes
 
+    //Fill form out with following info from table above this js file
     OptionsFill.appendChild(fill);
 }    
 )
 
-function buy()
+//Function will display subtotal prior to final payment
+function Subtotal()
 {
-    let total = 0;
+    //variable keeps track of subtotal
+    let subtotal = 0;
 
-    const tax = 0.0825; //local tax
-
+    //get quatity and price calculations
     Litem.forEach((item, index) =>
     {
         const qty = parseInt(document.getElementById(`qty${index}`).value) || 0;
+        subtotal += qty * item.tag;
+    });
+    //display subtotal on screen in live time when making changes
+    document.getElementById("sub").textContent = subtotal.toFixed(2);
+}
+//Function will display 
+function Quantity()
+{
+    //variable keeps track of subtotal
+    let items = 0;
+
+    //get quatity and price calculations
+    Litem.forEach((item, index) =>
+    {
+        const qty = parseInt(document.getElementById(`qty${index}`).value) || 0;
+        items += qty;
+    });
+    //display subtotal on screen in live time when making changes
+    document.getElementById("items").textContent = items;
+}
+//Function will calculate total of items purchased by user
+function buy()
+{
+    //gets total(after tax)
+    let total = 0;
+    //gets subtotal(before tax)
+    let subtotal = 0;
+    //local tax
+    const tax = 0.0825;
+    //Display item name
+    const ItemInfo = [];
+
+    Litem.forEach((item, index) =>
+    {
+        //Gets quantity
+        const qty = parseInt(document.getElementById(`qty${index}`).value) || 0;
+        //collects total calculating quantity and price tag of item
         total += qty * item.tag;
+        //Make sure only selected items are displayed
+        if(qty > 0)
+        {
+            //Count number of item lines for final display
+            const count = ItemInfo.filter(Boolean).length + 1;
+            //push item name and price into ItemInfo display array in this format
+            ItemInfo.push(`${count}-${item.option} (${qty}): $${item.tag}\n`);
+        }
     });
 
+    subtotal += total;
+    //gets total with taxes when paying
     total += total * tax;
-    alert(`Your total (including taxes) is: $${total}`);
+    //.join("") will help format display on ItemInfo array by eliminating the commas
+    alert(`${ItemInfo.join("")}\nSubTotal (Before taxes): $${subtotal.toFixed(2)}\nYour total (After taxes): $${total.toFixed(2)}`);
 }
